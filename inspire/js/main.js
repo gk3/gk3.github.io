42ee7dc5dc5531e740d23ba449a8674a0d7909ac
George Kedenburg III <gk3@fb.com>
2018-04-20 08:26:24 -0700

    first shit

---
+++ inspire/js/main.js
@@ -0,0 +1,107 @@
+  var config = {
+    apiKey: "AIzaSyAoNWwsp9RaarHBeKwyl7diDA3VfUT-sGw",
+    authDomain: "inspireme-8655c.firebaseapp.com",
+    databaseURL: "https://inspireme-8655c.firebaseio.com",
+    projectId: "inspireme-8655c",
+    storageBucket: "inspireme-8655c.appspot.com",
+    messagingSenderId: "596009452969"
+  };
+  firebase.initializeApp(config);
+
+var GIF = []
+var tweets = []
+var getGIF = function(){
+	var request = new XMLHttpRequest();
+	request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=er6a877s34lCzAE3fK3czrKploSgnomp&tag=kanye&rating=PG-13', true);
+
+	request.onload = function() {
+			if (request.status >= 200 && request.status < 400) {
+					// Success!
+				GIF = JSON.parse(request.responseText);
+				console.log(GIF)
+				var gifSrc = GIF.data.image_url
+				 var image = new Image();
+
+				// this will occur when the image is successfully loaded
+				// no matter if seconds past
+				image.onload = function () {
+
+				var gifW = GIF.data.image_width
+				var gifH = GIF.data.image_height
+				var gifSrc = GIF.data.image_url
+     
+				var gifContainer = document.getElementById("gif")
+				console.log(gifContainer)
+				var thresh = 20
+				var xPos = 50 + getRandomArbitrary(-thresh, thresh);
+				var yPos = 50 + getRandomArbitrary(-thresh, thresh);
+				var scale = getRandomArbitrary(.8,1.5)
+				gifContainer.setAttribute("style", "width:" + gifW + "px; height:" + gifH + "px; background-image: url(" + gifSrc + "); transform: translateX(-" + xPos + "%) translateY(-" + yPos + "%) scale(" + scale  +")")
+					getTweet()
+				}
+				  	image.src = gifSrc;
+			} else {
+					// We reached our target server, but it returned an error
+
+			}
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
+
+}
+
+var database = firebase.database();
+
+firebase.database().ref('/').once('value').then(function(snapshot) {
+
+	tweets = randomNoRepeats(snapshot.val().tweets)
+	console.log("getting gif")
+
+});
+
+
+var getTweet = function(){
+	var newTweet = tweets()
+	var tweetContents = document.getElementById("tweetContent")
+	tweetContents.innerHTML = newTweet.content
+	tweetContents.parentElement.setAttribute('href', 'https://twitter.com/kanyewest/status/' + newTweet.id)
+					var thresh = 20
+				var xPos = 50 + getRandomArbitrary(-thresh, thresh);
+				var yPos = 50 + getRandomArbitrary(-thresh, thresh);
+				var scale = getRandomArbitrary(.8,1.5);
+	tweetContents.parentElement.setAttribute("style", "transform: translateX(-" + xPos + "%) translateY(-" + yPos + "%) scale(" + scale  +")")
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  getGIF()
+
+	var yeBtn = document.getElementById('yeButton');
+
+  //add event listener
+  yeBtn.addEventListener('click', function(event) {
+    getGIF()
+  });
+
+
+});
+
+
+
+var anotherOne = debounce(function () {
+  var headline = document.querySelector('h1')
+
+  headline.classList.add('intro')
+  var rand = list()
+  var url = rand.get('file').url()
+  var img = rand.get('img').url()
+  makeHeadlines(rand.get('text'))
+  var audio = document.querySelector('audio');
+  var ig = document.querySelector('#ig')
+  ig.setAttribute('href', img)
+  audio.setAttribute('src', url)
+  audio.play()
+}, 100, true)
---
+++ inspire/js/plugins.js
@@ -0,0 +1,95 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+function randomNoRepeats(array) {
+  var copy = array.slice(0);
+  return function() {
+    if (copy.length < 1) { copy = array.slice(0); }
+    var index = Math.floor(Math.random() * copy.length);
+    var item = copy[index];
+    copy.splice(index, 1);
+    return item;
+  };
+}
+
+(function() {
+  if (window.__twitterIntentHandler) return;
+  var intentRegex = /twitter\.com\/intent\/(\w+)/,
+      windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes',
+      width = 550,
+      height = 420,
+      winHeight = screen.height,
+      winWidth = screen.width;
+ 
+  function handleIntent(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+        m, left, top;
+ 
+    while (target && target.nodeName.toLowerCase() !== 'a') {
+      target = target.parentNode;
+    }
+ 
+    if (target && target.nodeName.toLowerCase() === 'a' && target.href) {
+      m = target.href.match(intentRegex);
+      if (m) {
+        left = Math.round((winWidth / 2) - (width / 2));
+        top = 0;
+ 
+        if (winHeight > height) {
+          top = Math.round((winHeight / 2) - (height / 2));
+        }
+ 
+        window.open(target.href, 'intent', windowOptions + ',width=' + width +
+                                           ',height=' + height + ',left=' + left + ',top=' + top);
+        e.returnValue = false;
+        e.preventDefault && e.preventDefault();
+      }
+    }
+  }
+ 
+  if (document.addEventListener) {
+    document.addEventListener('click', handleIntent, false);
+  } else if (document.attachEvent) {
+    document.attachEvent('onclick', handleIntent);
+  }
+  window.__twitterIntentHandler = true;
+}());
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
