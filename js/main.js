4cee8918cd0d8ead38d91eca783a6aa16d35d8a3
George Kedenburg III <gk3@fb.com>
2018-04-21 08:46:01 -0700

    refactored gif queue

--- js/main.js
+++ js/main.js
@@ -1,112 +1,109 @@
-  var config = {
-    apiKey: "AIzaSyAoNWwsp9RaarHBeKwyl7diDA3VfUT-sGw",
-    authDomain: "inspireme-8655c.firebaseapp.com",
-    databaseURL: "https://inspireme-8655c.firebaseio.com",
-    projectId: "inspireme-8655c",
-    storageBucket: "inspireme-8655c.appspot.com",
-    messagingSenderId: "596009452969"
-  };
-  firebase.initializeApp(config);
+var config = {
+ apiKey: "AIzaSyAoNWwsp9RaarHBeKwyl7diDA3VfUT-sGw",
+ authDomain: "inspireme-8655c.firebaseapp.com",
+ databaseURL: "https://inspireme-8655c.firebaseio.com",
+ projectId: "inspireme-8655c",
+ storageBucket: "inspireme-8655c.appspot.com",
+ messagingSenderId: "596009452969"
+};
+firebase.initializeApp(config);
 
 var GIF = []
 var tweets = []
-var getGIF = function(){
-	var request = new XMLHttpRequest();
-	request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=er6a877s34lCzAE3fK3czrKploSgnomp&tag=kanye%20west&rating=R', true);
+var currentGIF = null
+var nextGIF = null
 
-	request.onload = function() {
-			if (request.status >= 200 && request.status < 400) {
-					// Success!
-				GIF = JSON.parse(request.responseText);
-				console.log(GIF)
-				var gifSrc = GIF.data.image_url
-				var gifSrcLow = GIF.data.fixed_height_small_still_url
-				 var image = new Image();
 
-				
-								var gifW = GIF.data.image_width
-				var gifH = GIF.data.image_height
-				var gifSrc = GIF.data.image_url
-							var gifContainer = document.getElementById("gif")
-				var gifImg = document.getElementById("gifInner")
-				// this will occur when the image is successfully loaded
-				// no matter if seconds past
-				var thresh = 20
-				var xPos = 50 + getRandomArbitrary(-thresh, thresh);
-				var yPos = 50 + getRandomArbitrary(-thresh, thresh);
-				var scale = getRandomArbitrary(.8,1.5)
-				var color = colors()
-				gifContainer.setAttribute("style", "width:" + gifW + "px; height:" + gifH + "px; transform: translateX(-" + xPos + "%) translateY(-" + yPos + "%) scale(" + scale  +"); background-color: #" + color.gif + ";")
-				getTweet(color.text)
-				gifImg.setAttribute('src', '')
-				gifImg.setAttribute('src', gifSrcLow)
-				image.onload = function () {
-					console.log("loaded")
+var queueGIF = function () {
+ console.log("queueing")
+  if (nextGIF == null) {
+   nextGIF = {}
+   var request = new XMLHttpRequest();
+   request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=er6a877s34lCzAE3fK3czrKploSgnomp&tag=kanye%20west&rating=R', true);
+   request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
 
-     
-				var gifContainer = document.getElementById("gif")
-				var gifImg = document.getElementById("gifInner")
-				console.log(gifContainer)
-					var gifSrc = GIF.data.image_url
-					gifImg.setAttribute('src', gifSrc)
-					
-				}
-				image.src = gifSrc;
-			} else {
-					// We reached our target server, but it returned an error
-
-			}
-};
-
-request.onerror = function() {
-  // There was a connection error of some sort
-};
-
-request.send();
-
+     response = JSON.parse(request.responseText);
+     console.log(response)
+     nextGIF.preview = response.data.fixed_height_small_still_url
+     nextGIF.image = new Image();
+     nextGIF.image.src = response.data.image_url
+     nextGIF.width = response.data.image_width
+     nextGIF.height = response.data.image_height
+     if(currentGIF == null){
+       console.log("getting second")
+        getGIF()
+     }
+    }
+   }
+   request.send()
+  }
 }
 
-var database = firebase.database();
 
-firebase.database().ref('/').once('value').then(function(snapshot) {
-
-	tweets = randomNoRepeats(snapshot.val().tweets)
-	colors = randomNoRepeats(snapshot.val().colors)
-	console.log("getting gif")
-  getGIF()
-});
-
-
-var getTweet = function(color){
-	var newTweet = tweets()
-	var tweetContents = document.getElementById("tweetContent")
-	tweetContents.innerHTML = newTweet.content
-	tweetContents.parentElement.setAttribute('href', 'https://twitter.com/kanyewest/status/' + newTweet.id)
-	tweetContents.parentElement.classList.remove("loading")
-//	tweetContents.parentElement.setAttribute('style', 'color: #' + color + ";")
-					var thresh = 20
-				var xPos = 50 + getRandomArbitrary(-thresh, thresh);
-				var yPos = 50 + getRandomArbitrary(-thresh, thresh);
-				var scale = getRandomArbitrary(.8,1.5);
-	tweetContents.parentElement.setAttribute("style", "color: #" + color + "; transform: translateX(-" + xPos + "%) translateY(-" + yPos + "%) scale(" + scale  +")")
+var getGIF = function () {
+ console.log(nextGIF)
+ if (nextGIF == null) {
+  console.log("queueing first")
+  queueGIF()
+ } else {
+  console.log("got one, setting", nextGIF)
+  currentGIF = nextGIF
+  nextGIF = null
+  queueGIF()
+  var gifContainer = document.getElementById("gif")
+  var gifImg = document.getElementById("gifInner")
+  var thresh = 20
+  var xPos = 50 + getRandomArbitrary(-thresh, thresh);
+  var yPos = 50 + getRandomArbitrary(-thresh, thresh);
+  var scale = getRandomArbitrary(.8, 1.5)
+  var color = colors()
+  gifContainer.setAttribute("style", "width:" + currentGIF.width + "px; height:" + currentGIF.height + "px; transform: translateX(-" + xPos + "%) translateY(-" + yPos + "%) scale(" + scale + "); background-color: #" + color.gif + ";")
+  getTweet(color.text)
+  gifImg.setAttribute('src', '')
+  gifImg.setAttribute('src', currentGIF.image.src)
+ }
 }
-var audio = new Audio('https://github.com/gk3/gk3.github.io/raw/master/inspire/mp3/yebtn-micro.mp3');
-audio.volume = .5
-
-document.addEventListener("DOMContentLoaded", function () {
 
 
-	var yeBtn = document.getElementById('yeButton');
+  var database = firebase.database();
 
-  //add event listener
-  yeBtn.addEventListener('click', function(event) {
+  firebase.database().ref('/').once('value').then(function (snapshot) {
 
-			audio.play();
-    getGIF()
+   tweets = randomNoRepeats(snapshot.val().tweets)
+   colors = randomNoRepeats(snapshot.val().colors)
+   console.log("getting gif")
+   getGIF()
   });
 
 
-});
+  var getTweet = function (color) {
+   var newTweet = tweets()
+   var tweetContents = document.getElementById("tweetContent")
+   tweetContents.innerHTML = newTweet.content
+   tweetContents.parentElement.setAttribute('href', 'https://twitter.com/kanyewest/status/' + newTweet.id)
+   tweetContents.parentElement.classList.remove("loading")
+   //	tweetContents.parentElement.setAttribute('style', 'color: #' + color + ";")
+   var thresh = 20
+   var xPos = 50 + getRandomArbitrary(-thresh, thresh);
+   var yPos = 50 + getRandomArbitrary(-thresh, thresh);
+   var scale = getRandomArbitrary(.8, 1.5);
+   tweetContents.parentElement.setAttribute("style", "color: #" + color + "; transform: translateX(-" + xPos + "%) translateY(-" + yPos + "%) scale(" + scale + ")")
+  }
+  var audio = new Audio('https://github.com/gk3/gk3.github.io/raw/master/inspire/mp3/yebtn-micro.mp3');
+  audio.volume = .5
+
+  document.addEventListener("DOMContentLoaded", function () {
 
 
+   var yeBtn = document.getElementById('yeButton');
 
+   //add event listener
+   yeBtn.addEventListener('click', function (event) {
+
+    audio.play();
+    getGIF()
+   });
+
+
+  });
