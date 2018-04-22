574448269fd0c3842035573d072c921e70cdde97
George Kedenburg III <gk3@fb.com>
2018-04-22 10:21:16 -0700

    more mobile styling

--- js/main.js
+++ js/main.js
@@ -75,11 +75,11 @@ var getGIF = function () {
   var color = colors()
   var flipped = false
   if (!Math.floor(Math.random() * 2)) {
-//   xPos *= -1
+   //   xPos *= -1
    flipped = true
-  } 
+  }
   gifContainer.setAttribute("style", "width:" + currentGIF.width + "px; height:" + currentGIF.height + "px; transform: translateX(" + xPos + "%) translateY(-" + yPos + "%) scale(" + scale + ");")
-//  xPos -= getRandomArbitrary(20, 40);
+  //  xPos -= getRandomArbitrary(20, 40);
   yPos = getRandomArbitrary(-60, 30);
   if (xPos > -20) {
    xPos = getRandomArbitrary(-100, -20);
@@ -93,7 +93,7 @@ var getGIF = function () {
   gifZoom.setAttribute('poster', currentGIF.preview)
   var cachebuster = Math.round(new Date().getTime() / 1000);
   gifZoom.setAttribute('src', gifImg.src + "?cache=" + cachebuster)
-  scroll.setAttribute("style", "height:" + getRandomArbitrary(50,200) + "px; transform: translateX(" + getRandomArbitrary(-300, 300) + "px) translateY(" + getRandomArbitrary(-60, -40) + "%)")
+  scroll.setAttribute("style", "height:" + getRandomArbitrary(50, 200) + "px; transform: translateX(" + getRandomArbitrary(-300, 300) + "px) translateY(" + getRandomArbitrary(-60, -40) + "%)")
  }
 }
 
@@ -105,28 +105,35 @@ firebase.database().ref('/').once('value').then(function (snapshot) {
  tweets = randomNoRepeats(snapshot.val().tweets)
  colors = randomNoRepeats(snapshot.val().colors)
  updated = snapshot.val().updated
- 
+
  document.getElementById("date").innerHTML = updated
- 
+
  console.log("getting gif")
  getGIF()
 });
 
-
+var firstTweet = false
 var getTweet = function (color, x, y) {
+
  var newTweet = tweets()
  var tweetContents = document.getElementById("tweetContent")
  tweetContents.innerHTML = newTweet.content
  tweetContents.parentElement.setAttribute('href', 'https://twitter.com/kanyewest/status/' + newTweet.id)
-// tweetContents.parentElement.classList.remove("loading")
- var loading = document.querySelectorAll(".loading")
- loading.forEach(function(el) {
-  el.classList.remove("loading")
-})
+ // tweetContents.parentElement.classList.remove("loading")
+ if (!firstTweet) {
+  var loading = document.querySelectorAll(".loading")
+  loading.forEach(function (el) {
+   el.classList.remove("loading")
+  })
+  firstTweet = true;
+ }
+if(window.innerWidth	> 756){
+
+
  //	tweetContents.parentElement.setAttribute('style', 'color: #' + color + ";")
  var thresh = 20
- if (x > -20){
-   var xPos = x + getRandomArbitrary(-100, -20);
+ if (x > -20) {
+  var xPos = x + getRandomArbitrary(-100, -20);
  } else {
   var xPos = x + getRandomArbitrary(-20, 30);
  }
@@ -135,6 +142,10 @@ var getTweet = function (color, x, y) {
  var scale = getRandomArbitrary(.8, 1.1);
  tweetContents.parentElement.setAttribute("style", "color: #" + color + "; transform: translateX(" + xPos + "%) translateY(" + yPos + "%) scale(" + scale + ")")
 }
+ }
+
+
+
 var audio = new Audio('https://github.com/gk3/gk3.github.io/raw/master/mp3/ye-fade.mp3');
 audio.volume = .5
 
